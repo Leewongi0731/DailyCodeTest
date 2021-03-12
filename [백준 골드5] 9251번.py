a1cbcb8d894c06d8e463cbcecb6c9931ae7e4291
lee wongi <48685242+Leewongi0731@users.noreply.github.com>
2021-03-12 20:42:23 +0900

    2021_03_12 / 9251번: LCS

---
+++ [백준 골드5] 9251번.py
@@ -0,0 +1,15 @@
+# 9251번: LCS
+from bisect import bisect_left
+
+A = list( input() )
+B = list( input() )
+dp = [ [0 for i in range(len(B)+1)] for i in range(len(A)+1) ]
+
+for i in range(1, len(A)+1):
+    for j in range(1, len(B)+1):
+        if A[i-1] == B[j-1]:
+            dp[i][j] = dp[i-1][j-1] +1
+        else:
+            dp[i][j] = max( dp[i-1][j], dp[i][j-1] )
+
+print( dp[len(A)][len(B)] )
