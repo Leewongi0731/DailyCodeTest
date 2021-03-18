c37c7aec83c9a3e1357e23830709b32d8ab0dd10
Leewongi <wglee0731@naver.com>
2021-03-18 22:47:57 +0900

    2021_03_18 / 2042번: 구간 합 구하기

--- [백준 골드1] 2042번.py
+++ [백준 골드1] 2042번.py
@@ -1,4 +1,46 @@
 # 2042번: 구간 합 구하기
+
+# 풀이1 : FenwickTree이용
+def updateFenwickTree( i, diff ):
+    global N
+    while i <= N:
+        fenwickTree[i] += diff
+        i += (i & -i) # 마지막 1에 1추가
+        
+def getSumFenwickTree( i ):
+    ans = 0
+    while i > 0:
+        ans += fenwickTree[i]
+        i -= (i & -i) # 마지막 1제거
+    return ans
+
+#################################################################
+N, M, K = list(map(int, input().split()))
+datas = [0] + [ int(input()) for i in range(N) ]
+
+fenwickTree = [0 for i in range(N+1)]
+
+# init fenwickTree
+for i in range(1, N+1):
+    data = datas[i]
+    updateFenwickTree( i, data )
+
+
+for _ in range(M+K):
+    a,b,c = list(map(int, input().split()))
+    if a == 1:
+        beforData = datas[b]
+        afterData = c
+        
+        diff = afterData - beforData
+        updateFenwickTree( b, diff )
+        datas[b] = c
+    else:
+        rangeSum = getSumFenwickTree( c ) - getSumFenwickTree( b-1 )
+        print( rangeSum )
+
+'''
+# 풀이 2: Segment Tree이용
 def initTree( left, right, node ):
     if left==right: 
         tree[node] = datas[left]
@@ -42,3 +84,4 @@ for _ in range(M+K):
     else:
         targetLeft, targetRight = b, c
         print( getRangeSum( 1, N, 1 ) )
+'''
